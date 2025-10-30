4007af62c3ab2f3e875af8cf5810562c3b57e79b
Theodore Olsauskas-Warren <6678321+Sauski@users.noreply.github.com>
2025-10-30 11:35:37 -0700

    Add minivim for current scope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,6 +23,16 @@ require('nvim-treesitter.configs').setup({
   },
 })
 
+-- mini.indentscope configuration
+require('mini.indentscope').setup({
+  symbol = '│',
+  options = { try_as_border = true },
+  draw = {
+    delay = 0,
+    animation = require('mini.indentscope').gen_animation.cubic({ easing = 'in-out', duration = 30, unit = 'total' }),
+  },
+})
+
 -- fzf-lua Configuration
 require("fzf-lua").setup({
   keymap = {
