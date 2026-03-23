1d08c354dfe8c5042e55147303ee83bcd21b3ea1
Theodore Olsauskas-Warren <6678321+Sauski@users.noreply.github.com>
2026-03-22 19:54:29 -0700

    Update appearance.lua

--- .config/nvim/lua/appearance.lua
+++ .config/nvim/lua/appearance.lua
@@ -7,3 +7,4 @@ vim.api.nvim_set_hl(0, "@type.builtin", { fg = "#e78a4e" })
 
 vim.g.neovide_scroll_animation_length = 0.3
 vim.g.neovide_cursor_animation_length = 0.1
+vim.g.neovide_cursor_animate_command_line = false
